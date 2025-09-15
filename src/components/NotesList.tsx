facf0a2f6e9c46dabbbfa6ed922b520dfb866916
faizbinyousuf <faizbinyousaf@gmail.com>
2025-09-15 23:47:53 +0400

    fixed scrolling issue in note list

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,25 +1,25 @@
-import NoteItem from "./NoteItem";
-import FloatingActionButton from "./Fab";
-import { Button } from "./ui/button";
 import { PlusIcon } from "lucide-react";
+import FloatingActionButton from "./Fab";
+import NoteItem from "./NoteItem";
+import { Button } from "./ui/button";
 
 function NotesList() {
   return (
-    <div className="bg-white overflow-y-scroll [&::-webkit-scrollbar]:w-[3px] [&::-webkit-scrollbar-thumb]:bg-gray-300   h-full w-full  xl:px-4 lg:px-1   border border-gray-200 lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3">
-      Notelist
-      {/* <Button
-        className="lg:hidden fixed bottom-20 right-4 rounded-full p-7 shadow-lg z-50 bg-blue-600"
-        size="icon"
-      >
-        <PlusIcon className="size-6" />
-        Create Note
-      </Button> */}
-      {Array(3)
-        .fill(0)
-        .map((_, index) => (
-          <NoteItem key={index} />
-        ))}
-      {/* <FloatingActionButton /> */}
+    <div className="bg-white overflow-y-auto [&::-webkit-scrollbar]:w-[3px] [&::-webkit-scrollbar-thumb]:bg-gray-300  w-full  xl:px-4 lg:px-1   border border-gray-200 lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3 ">
+      <div className="flex flex-col h-screen">
+        <Button className=" bg-blue-600 mx-3 mt-4 rounded-sm text-white hover:bg-blue-800">
+          <PlusIcon className="" />
+          Add Note
+        </Button>
+        <div className="flex-1 ">
+          {Array(10)
+            .fill(0)
+            .map((_, index) => (
+              <NoteItem key={index} />
+            ))}
+        </div>
+        <FloatingActionButton />
+      </div>
     </div>
   );
 }
